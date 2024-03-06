545cedd311635924b715a0960765ec8736b8e029
Rodrigo-Haupper <rodrigo.costa@haupper.pt>
2024-03-06 17:40:27 +0000

    Project commit

---
+++ AirbnbAPI/src/test/java/isec/airbnbapi/AirbnbApiApplicationTests.java
@@ -0,0 +1,13 @@
+package isec.airbnbapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirbnbApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
